d5a69bed5a2da7cdc5826996f714eae5ccc377e1
沛彪 林 <linpeibiao2022@163.com>
2023-05-30 18:47:35 +0800

    refactor: 拦截器

--- src/main/java/com/xiaohu/media_assets/intercepter/LoginInterceptor.java
+++ src/main/java/com/xiaohu/media_assets/intercepter/LoginInterceptor.java
@@ -36,26 +36,22 @@ public class LoginInterceptor implements HandlerInterceptor {
         if (url.contains("login") || url.contains("sendCode") || url.contains("v1/minio/tasks")){
             return true;
         }
-        // 1.判断是否需要拦截（ThreadLocal中是否有用户）
-        if (UserHolder.get() == null) {
-            log.info("不存在登录用户");
-            String token = request.getHeader("token");
-            if (StringUtils.isBlank(token)){
-                throw new BusinessException(ResultCode.NOT_LOGIN, "未登录");
-            }
-            String tokenKey = LOGIN_USER_KEY + token;
-            // 先从 redis 中拿到登录信息，若数据为空，返回false
-            Map<Object, Object> userMap = stringRedisTemplate.opsForHash().entries(tokenKey);
-            // 判断 userMap
-            if (userMap.isEmpty()){
-                throw new BusinessException(ResultCode.NOT_LOGIN, "未登录");
-            }
-            // 将 map 转换成 User 实体
-            User user = BeanUtil.fillBeanWithMap(userMap, new User(), false);
-            // 将 USer 保存在 UserHolder
-            UserHolder.save(user);
-            return true;
+        // 1.判断是否需要拦截（缓存中是否有用户）
+        String token = request.getHeader("token");
+        if (StringUtils.isBlank(token)){
+            throw new BusinessException(ResultCode.NOT_LOGIN, "未登录");
         }
+        String tokenKey = LOGIN_USER_KEY + token;
+        // 先从 redis 中拿到登录信息，若数据为空，返回false
+        Map<Object, Object> userMap = stringRedisTemplate.opsForHash().entries(tokenKey);
+        // 判断 userMap
+        if (userMap.isEmpty()){
+            throw new BusinessException(ResultCode.NOT_LOGIN, "未登录");
+        }
+        // 将 map 转换成 User 实体
+        User user = BeanUtil.fillBeanWithMap(userMap, new User(), false);
+        // 将 USer 保存在 UserHolder
+        UserHolder.save(user);
         // 有用户，则放行
         log.info("存在登录用户");
         return true;
